1d8b297f0228df1c45c1dcf13262ee27c747ed4f
SantiagoPacheco99 <161869143+SantiagoPacheco99@users.noreply.github.com>
2024-03-01 19:30:10 +0000

    guardado

---
+++ entrega/guia/guiaI.py
@@ -0,0 +1,27 @@
+#Punto a
+print("Las maquinas me sorprenden con mucha frecuencia")
+
+#Punto b
+print()
+
+#Punto c
+print("Esto es un mensaje")
+
+#Punto d
+print("23")
+print(23)
+
+#Punto e
+print("Una computadora puede ser llamada \"inteligente\" si logra engañar a una persona haciendole creer que es un humano")
+print("texto \"Destacado\" sigo con el texto")
+print('oracion comillas simples')
+print("usando comillas dobles")
+#print('No esta bien")
+print('texto "destacado" sigo con el texto')
+print("texto 'destacado' sigo con el texto")
+
+#Punto f
+print("Santiago","Pacheco","21 años")
+
+#Punto g
+print("Santiago","Pacheco","21 años",sep="_")
