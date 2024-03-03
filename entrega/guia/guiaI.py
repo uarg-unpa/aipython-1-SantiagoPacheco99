d84914bbae684444604c451a0eb9d7e7eaf4c7be
SantiagoPacheco99 <161869143+SantiagoPacheco99@users.noreply.github.com>
2024-03-03 00:48:21 +0000

    g

--- entrega/guia/guiaI.py
+++ entrega/guia/guiaI.py
@@ -32,4 +32,36 @@ print("calle","numero","codigo postal",sep="\t")
 #Punto i
 print("calle","numero","codigo postal",sep="\n")
 
+print()
+
 #Punto j
+print("Feliz","Primavera","\t\t2024",sep="\n \t")
+
+#Punto k
+print("Solo podemos ver poco del futuro", end=",")
+print(" pero lo suficiente para darnos cuenta de que hay mucho que hacer")
+print()
+
+#Punto l
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+
+#Punto 2
+
+print(5+7)
+print(0-9)
+print(2*45)
+print(3/2)
+print(6**2)
+print(3//2)
+
+print()
+x = int(input("Ingrese un valor para x: "))
+resultado = (x**2) + (6*x) + 9
+print("El resultado de ",x,"^2 +6*",x,"+9 es: ",resultado)
