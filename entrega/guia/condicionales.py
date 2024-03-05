df4fa52445c406e880ebbfc0d1aee39a4772a37f
SantiagoPacheco99 <161869143+SantiagoPacheco99@users.noreply.github.com>
2024-03-05 19:29:57 +0000

    g

--- entrega/guia/condicionales.py
+++ entrega/guia/condicionales.py
@@ -1,8 +1,20 @@
 #Sentencia if
-
+"""
 edad = int(input("Ingrese su edad: "))
 
 if(edad >= 18):
     print("Usted debe votar")
 else:
     print("Usted es menor de 18 años")
+"""
+
+calificacion=int(input("Ingrese calificacion: "))
+
+if calificacion >=90:
+    print("Excelente")
+elif calificacion >=80:
+    print("Muy bien")
+elif calificacion >=70:
+    print("Bien")
+else:
+    print("Insuficiente")
