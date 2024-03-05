62d47f4b2738eaa25436da0349e0dc7048fe69f2
SantiagoPacheco99 <161869143+SantiagoPacheco99@users.noreply.github.com>
2024-03-05 21:25:02 +0000

    s

--- entrega/guia/guiaII.py
+++ entrega/guia/guiaII.py
@@ -1,4 +1,7 @@
 #Punto 1
 edad = int(input("Ingrese su edad: "))
 if edad >= 18:
-    print()
+    print("Tiene edad suficiente para conducir")
+else:
+    print(f"Le faltan {18-edad} años para poder conducir")
+
