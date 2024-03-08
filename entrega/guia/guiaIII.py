e06a7e796372d18f4d956ddbe48530663a90ee03
SantiagoPacheco99 <161869143+SantiagoPacheco99@users.noreply.github.com>
2024-03-08 03:17:42 +0000

    s

--- entrega/guia/guiaIII.py
+++ entrega/guia/guiaIII.py
@@ -6,15 +6,23 @@ while(i<=100):
     i = i + 1
 """
 
+"""
 #Punto 2
+for num in range(0,101):
+    print(num, end=" ")
+"""
 
-
+"""
 #Punto 3
 i = int(10)
 while(i>=0):
     print(i)
     i = i - 1
 
+
+for num in range(10,-1,-1):
+    print(num)
+"""
 
 
 
