efd72d2d997704fa523f89a0f76fd46f65273ee8
SantiagoPacheco99 <161869143+SantiagoPacheco99@users.noreply.github.com>
2024-03-12 19:20:46 +0000

    s

--- entrega/guia/funciones.py
+++ entrega/guia/funciones.py
@@ -1,7 +1,9 @@
+"""
 def mensaje():
     print("Hola - AIPython")
 
 mensaje()
+"""
 
 """
 def suma():
@@ -17,9 +19,18 @@ def suma(num1,num2):
 
 print(suma(7,3))
 """
-
+"""
 def presentacion (nombre, apellido):
     print(f"Su nombre es: {nombre}, y su apellido es: {apellido}")
 
 presentacion("Santiago","Pacheco")
 presentacion(nombre="Santiago",apellido="Pacheco")
+"""
+
+
+def suma(num1=2, num2=4):
+    return num1 + num2
+
+print(suma()) #Devuelve 6
+
+
