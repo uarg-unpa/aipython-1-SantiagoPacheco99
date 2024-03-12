a943533c4bda7dd08c72de946ae4365c919616d4
SantiagoPacheco99 <161869143+SantiagoPacheco99@users.noreply.github.com>
2024-03-12 18:55:29 +0000

    g

---
+++ entrega/guia/funciones.py
@@ -0,0 +1,19 @@
+def mensaje():
+    print("Hola - AIPython")
+
+mensaje()
+
+"""
+def suma():
+    return (4 + 6)
+
+#suma() #Devuelve 10
+#suma(3) #Devuelve Error porque no fue definido en la funcion
+"""
+
+
+def suma(num1,num2):
+    return num1 + num2
+
+print(suma(7,3))
+
