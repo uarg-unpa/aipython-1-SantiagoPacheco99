c36842a388a2fa022f0f76b4a4a8944de365744a
SantiagoPacheco99 <161869143+SantiagoPacheco99@users.noreply.github.com>
2024-03-12 22:27:59 +0000

    s

--- entrega/guia/guiaIV.py
+++ entrega/guia/guiaIV.py
@@ -7,6 +7,38 @@ def producto(num1,num2):
 print(producto(4,5))
 """
 
+"""
+#Punto 4
+
+def tabla_multiplicar(num):
+    for i in range(1,11):
+       resultado = num * i
+       print(f"{num} x {i} = {resultado}")
+
+numero = int(input("Ingrese el numero para ver su tabla de multiplicar: "))
+print(tabla_multiplicar(numero))
+"""
+
+"""
+#Punto 5
+
+def par_impar(n):
+    if n % 2 == 0:
+        print(f"El numero {n} es par")
+    else:
+        print(f"El numero {n} es impar")
+
+
+num = int(input("Ingrese un numero para determinar si es par o impar: "))
+print(par_impar(num))
+"""
+
+
+
+
+
+
+
 
 
 
