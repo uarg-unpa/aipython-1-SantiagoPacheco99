b164d04dee0214e1a4e9c0aa466bd696da3e1e25
SantiagoPacheco99 <161869143+SantiagoPacheco99@users.noreply.github.com>
2024-03-15 19:16:47 +0000

    s

---
+++ entrega/guia/guiaV.py
@@ -0,0 +1,37 @@
+"""
+#Punto 1
+lista = []
+
+#Punto 2
+
+l = [1,2,3,4,5,6,7]
+"""
+"""
+#Punto 3
+
+lis = [1,2,3]
+print(len(lis))
+"""
+
+#Punto 4
+frutas = ["Frutilla","Manzana","Banana","Durazno"]
+print(len(frutas))
+print(frutas)
+
+frutas.append("Pera")
+print(frutas)
+
+
+"""
+#Punto 6
+
+datos_personales = ["Santiago",21,1.74,"soltero","Las Piedras 226"]
+"""
+
+"""
+#Punto 11
+numeros = [1,2,3,4,5,6,7,8,9,10]
+sub_num = numeros[:3]
+print(sub_num)
+"""
+
