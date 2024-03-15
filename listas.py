a8db27f7d2f279d5f5aead09927ec3e959011e51
SantiagoPacheco99 <161869143+SantiagoPacheco99@users.noreply.github.com>
2024-03-15 18:31:37 +0000

    s

---
+++ listas.py
@@ -0,0 +1,24 @@
+#Listas
+#Creacion lista vacia
+
+nombres = []
+
+#Valores iniciales
+
+nombres = ["Santiago","Joaquin",21,9,12,"Pacheco"]
+
+#Mostrar lista
+print(nombres)
+
+#Iterar sobre la lista usando indices
+for i in range(len(nombres)):
+    print(nombres[i])
+
+#Accedemos a los elementoa
+
+#Accedemos al primer elemento
+primerElemento = nombres[0]
+
+print(f"El primer elemento es: {primerElemento}")
+
+
