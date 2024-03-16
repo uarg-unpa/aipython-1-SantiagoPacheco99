23fcde083dc0977930adf2d2df509c665c043196
SantiagoPacheco99 <161869143+SantiagoPacheco99@users.noreply.github.com>
2024-03-16 00:16:09 +0000

    l

--- entrega/guia/guiaV.py
+++ entrega/guia/guiaV.py
@@ -6,6 +6,7 @@ lista = []
 
 l = [1,2,3,4,5,6,7]
 """
+
 """
 #Punto 3
 
@@ -22,7 +23,13 @@ frutas.pop(0)
 frutas.append("Pera")
 print(frutas)
 """
+"""
+#Punto 5
+lis = [1,2,3,4,5]
+print(lis.pop(0))
 
+print(lis.pop(-1))
+"""
 """
 #Punto 6
 
@@ -59,3 +66,27 @@ sub_num = numeros[:3]
 print(sub_num)
 """
 
+"""
+#Punto 14
+
+def lista_union(lista1,lista2):
+    lista = [lista1,lista2]
+    return lista
+
+nombres = ["Santiago", "Lionel", "Julian", "Ignacio"]
+apellidos = ["Pacheco", "Messi","Alvarez","Scocco"]
+print(lista_union(nombres,apellidos))
+"""
+
+"""
+#Punto 15
+def promedio(lista):
+    suma = 0
+    for i in lista:
+        suma = suma + i
+    prom = suma/len(lista)
+    return prom
+
+numeros = [1,2,3,4,5,6,7,8,9,10]
+print(f"Promedio: {promedio(numeros)}")
+"""
