bd26e91ef88010939755e67f96f6054d75f18541
SantiagoPacheco99 <161869143+SantiagoPacheco99@users.noreply.github.com>
2024-03-29 07:50:08 +0000

    s

---
+++ simulador/final.py
@@ -0,0 +1,44 @@
+import random
+
+#print(random.randint(1,6))
+
+print("****************************************************")
+print("*** BIENVENIDOS AL JUEGO DE LANZAMIENTO DE DADOS ***")
+print("****************************************************")
+print("")
+print("1er Paso - Debe elegir algun tipo de dado para jugar")
+cant_dados = int(input("Ingrese la cantidad de dados para lanzar: "))
+print()
+print("2do Paso - Debe elegir")
+caras=int(input("Ingrese la cantidad de caras de los dados: "))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
