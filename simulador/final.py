573920172a7737b03feeb33807cd31a90f30d9d9
SantiagoPacheco99 <161869143+SantiagoPacheco99@users.noreply.github.com>
2024-04-02 07:00:22 +0000

    s

--- simulador/final.py
+++ simulador/final.py
@@ -1,53 +1,93 @@
 import random
 
 
-print("****************************************************")
-print("*** BIENVENIDOS AL JUEGO DE LANZAMIENTO DE DADOS ***")
-print("****************************************************")
-print("")
-print("Antes de jugar debe definir lo siguiente: ")
-print("1-Cantidad de dados para jugar")
-print("2-Cantidad de caras que tendran los dados")
-print("3-Ingresar nombre de los jugadores")
-
-def lanzamiento(caras): #Metodo que retorna un numero al azar entre el 1 y las caras definidas por el usuario
+#Metodo que retorna un numero al azar entre el 1 y las caras definidas por el usuario
+def lanzamiento(caras): 
     return random.randint(1,caras)
 
 
-cant_dados = int(input("Ingrese la cantidad de dados para jugar: "))
+def lanzamientos_dados(dados, caras, lanzamientos, result):
+    print("Ingrese l para lanzar o s para salir")
+    lanzar = input("Lanzar: ")
+    for l in range(lanzamientos):
+        if(lanzar == "l"):
+            print(f"Lanzamiento N°{l + 1}:")
+            for d in range(dados):
+                dado = lanzamiento(caras)
+                print(f"Dado N°{d + 1}: {dado}")
+                result.append(dado)
+            lanzar = input("Presiona l para lanzar de nuevo o s para salir: ")
+            if (lanzar == "s"):
+                break
+        elif (lanzar == "s"):
+            break
+    return result
 
-def ingresar_jugadores(cant,jug):
-    for i in range(cant):
-        jug.append()
+def total_resultado(result):
+    suma = 0
+    for i in result:
+        suma = suma + i 
+    return suma
 
-def mostrar_jugadores(jug):
-    for j in jug:
-        return j
+def recorrer_tiros(tiros):
+    for t in tiros:
+        print(t,end="-")
 
-def juego():
-    jugadores = []
-    print("[1]-Para ver las reglas del juego")
-    print("[2]-Para empezar a jugar")
-    print("[3]-Para mostrar las estadisticas de los jugadores")
-    print("[4]-Para mostrar los jugadores")
-    while(op != 0):
+def juego_dados():
+    print("****************************************************")
+    print("*** BIENVENIDOS AL JUEGO DE LANZAMIENTO DE DADOS ***")
+    print("****************************************************")
+    print("")
+    print("Antes de jugar debe definir lo siguiente: ")
+    print("1-Cantidad de dados para jugar")
+    print("2-Cantidad de caras que tendran los dados")
+    print("3-Ingresar cantidad de tiros")
+    print("4-Ingresar nombre del jugador")
+
+
+    cant_dados = int(input("Ingrese la cantidad de dados para jugar: "))
+    caras = int(input("Ingrese la cantidad de caras que tendran los dados: "))
+    cant_tiros = int(input("Ingrese la cantidad de tiros que tendra el jugador: "))
+    jugador = input("Ingrese el nombre del jugador: ")
+    resultados = []
+    op = -1
+    print()
+    while(op!=0):
+        print("[1]-Para ver las reglas del juego")
+        print("[2]-Para empezar a jugar")
+        print("[3]-Para mostrar las estadisticas del jugador")
         op = int(input("Ingrese la opcion: "))
         match op:
             case 1:
-
+                print("")
+                print("\t****REGLAS****")
+                print(f"1-El jugador tiene {cant_tiros} cantidad de tiros de dados")
+                print(f"2-El jugador en cada tiro tendrá {cant_dados} dados para lanzar")
+                print("3-Cuando un jugador termine de lanzar, se sumará el resultado de sus tiros")
+                print("\t****¡¡DIVIERTETE!!****")
+                print("")
             case 2:
-                
+                print("")
+                print("\t****COMIENZA EL JUEGO****")
+                lanzamientos_dados(cant_dados,caras,cant_tiros,resultados)
+                print("\t****JUEGO TERMINADO****")
+                print("")
             case 3:
-
-            case 4:
-                print(f"Jugador:{mostrar_jugadores(jugadores)}")
+                print("****ESTADISTICAS****")
+                print(f"Estadisticas del jugador {jugador}:")
+                print("")
+                print("Tiros: ",end="")
+                recorrer_tiros(resultados)
+                print("")
+                print(f"Total: {total_resultado(resultados)}")
+                print("")
             case 0:
                 print("Usted ha salido del juego")
             case _:
-                print("Error. Opcion no existente")
+                print("Error. Opcion no existente, vuelva a ingresar una opcion!!")
 
 
-juego()
+juego_dados()
 
 
 
